3c9ef4ec8b7a9c3ccdcfa1ae2c3ec75232d52fa6
Min Pyae <minmp2002@gmail.com>
2024-01-26 10:26:01 +0000

    Initial Commit

---
+++ SwiftChat/SwiftChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChatApp.swift
+//  SwiftChat
+//
+//  Created by Chris Min on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
