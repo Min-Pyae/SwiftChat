6060ed94b680823bf5fd7d10056649d5add58285
Min Pyae <minmp2002@gmail.com>
2024-01-26 10:30:32 +0000

    Completed SwiftChat Projects Setup

---
+++ SwiftChat/View/Authentication/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  SwiftChat
+//
+//  Created by Chris Min on 26/01/2024.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
