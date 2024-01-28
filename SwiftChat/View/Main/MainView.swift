636f153e4a1bda0db720c46aaa66ed87dcf57913
Min Pyae <minmp2002@gmail.com>
2024-01-28 18:16:32 +0000

    Created NavigationTitle for each TabView

--- SwiftChat/View/Main/MainView.swift
+++ SwiftChat/View/Main/MainView.swift
@@ -8,34 +8,60 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @State var selectedIndex = 0
+    
     var body: some View {
         
         NavigationView {
             
             // TABVIEW
-            TabView {
+            TabView(selection: $selectedIndex) {
                 
                 // CHAT TAB ITEM
                 ChatsView()
+                    .onTapGesture {
+                        selectedIndex = 1
+                    }
                     .tabItem {
                         Image(systemName: "bubble.left")
                     }
+                    .tag(1)
                 
                 
                 // CHANNELS TAB ITEM
                 ChannelsView()
+                    .onTapGesture {
+                        selectedIndex = 2
+                    }
                     .tabItem {
                         Image(systemName: "bubble.left.and.bubble.right")
                     }
+                    .tag(2)
                 
                 
                 // SETTINGS TAB ITEM
                 SettingsView()
+                    .onTapGesture {
+                        selectedIndex = 3
+                    }
                     .tabItem {
                         Image(systemName: "gear")
                     }
+                    .tag(3)
             }
+            .navigationTitle(title)
         }
+    }
+    
+    var title: String {
+        switch selectedIndex {
+        case 1: return "Chats"
+        case 2: return "Channels"
+        case 3: return "Settings"
+        default: return ""
+        }
+        
     }
 }
 
